e9706a32e56556bb9c41143e0e7b7e8d8a52ccd0
Adam <ciesielskiadam6@gmail.com>
2026-01-05 21:57:40 +0100

    Poprawna konfiguracja SQLite i pierwsza migracja

--- CarRentalProject/Program.cs
+++ CarRentalProject/Program.cs
@@ -1,13 +1,14 @@
 using CarRentalProject.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
